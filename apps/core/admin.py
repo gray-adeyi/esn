19271a59e114c37e07b07e23c289785853b94ef8
Gbenga Adeyi <adeyigbenga005@gmail.com>
2021-10-26 19:58:22 -0400

    Add models to admin site and prepare landing page for preview

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -4,3 +4,8 @@ from . import models
 # Register your models here.
 admin.site.register(models.FAQ)
 admin.site.register(models.Site)
+admin.site.register(models.SiteSocialAccount)
+admin.site.register(models.TeamLead)
+admin.site.register(models.TeamLeadSocialAccount)
+admin.site.register(models.Event)
+admin.site.register(models.PhoneNumber)
